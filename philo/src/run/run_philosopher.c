4e0d224fd6cd19e862d775c3c04859b34bbb5f03
MyNameIsTrez <sbos@student.codam.nl>
2023-01-18 15:20:20 +0100

    Refactor run_regular_philosopher()

--- philo/src/run/run_philosopher.c
+++ philo/src/run/run_philosopher.c
@@ -16,7 +16,7 @@ static void	run_single_philosopher_edgecase(t_philosopher *philosopher,
 				t_data *data)
 {
 	mutex_lock(&data->running_program_mutex);
-	if (!data->running_program) // TODO: Is this one necessary?
+	if (!data->running_program)
 	{
 		mutex_unlock(&data->running_program_mutex);
 		return ;
@@ -38,78 +38,83 @@ static void	run_single_philosopher_edgecase(t_philosopher *philosopher,
 	mutex_unlock(philosopher->left_fork);
 }
 
+static bool	should_stop(t_data *data)
+{
+	mutex_lock(&data->running_program_mutex);
+	if (!data->running_program) // TODO: Should this check be done after every action below?
+	{
+		mutex_unlock(&data->running_program_mutex);
+		return (true);
+	}
+	mutex_unlock(&data->running_program_mutex);
+	return (false);
+}
+
+static void	grab_forks(t_philosopher *philosopher, t_data *data)
+{
+	if (philosopher->index % 2 == 0)
+	{
+		mutex_lock(philosopher->left_fork);
+		print_event(EVENT_FORK, philosopher, data);
+		mutex_lock(philosopher->right_fork);
+		print_event(EVENT_FORK, philosopher, data);
+	}
+	else
+	{
+		mutex_lock(philosopher->right_fork);
+		print_event(EVENT_FORK, philosopher, data);
+		mutex_lock(philosopher->left_fork);
+		print_event(EVENT_FORK, philosopher, data);
+	}
+}
+
+void	eat(t_philosopher *philosopher, t_data *data)
+{
+	philosopher->times_eaten++;
+	if (philosopher->times_eaten == data->times_to_eat)
+	{
+		mutex_lock(&data->philosophers_eating_mutex);
+		data->philosophers_eating--;
+		mutex_unlock(&data->philosophers_eating_mutex);
+	}
+	print_event(EVENT_EAT, philosopher, data);
+	mutex_lock(&philosopher->time_of_last_meal_mutex);
+	philosopher->time_of_last_meal = get_time_ms();
+	mutex_unlock(&philosopher->time_of_last_meal_mutex);
+	precise_sleep(philosopher->time_of_last_meal, data->time_to_eat, data);
+}
+
+void	drop_forks(t_philosopher *philosopher)
+{
+	mutex_unlock(philosopher->left_fork);
+	mutex_unlock(philosopher->right_fork);
+}
+
+void	ph_sleep(t_philosopher *philosopher, t_data *data)
+{
+	print_event(EVENT_SLEEP, philosopher, data);
+	precise_sleep(get_time_ms(), data->time_to_sleep, data);
+}
+
+void	think(t_philosopher *philosopher, t_data *data)
+{
+	print_event(EVENT_THINK, philosopher, data);
+}
+
 static void	run_regular_philosopher(t_philosopher *philosopher, t_data *data)
 {
 	if (philosopher->index % 2 == 1)
-		precise_sleep(philosopher->time_of_last_meal, data->time_to_eat / 2, data);
-
+		precise_sleep(philosopher->time_of_last_meal, data->time_to_eat / 2,
+			data);
 	while (true)
 	{
-		mutex_lock(&data->running_program_mutex);
-		if (!data->running_program) // TODO: Should this check be done after every action below?
-		{
-			mutex_unlock(&data->running_program_mutex);
-			break ;
-		}
-		mutex_unlock(&data->running_program_mutex);
-
-		if (philosopher->index % 2 == 0)
-		{
-			mutex_lock(philosopher->left_fork);
-
-			print_event(EVENT_FORK, philosopher, data);
-
-			mutex_lock(philosopher->right_fork);
-
-			print_event(EVENT_FORK, philosopher, data);
-		}
-		else
-		{
-			mutex_lock(philosopher->right_fork);
-
-			print_event(EVENT_FORK, philosopher, data);
-
-			mutex_lock(philosopher->left_fork);
-
-			print_event(EVENT_FORK, philosopher, data);
-		}
-
-		// TODO: This probably shouldn't happen when the philosopher hasn't eaten but is just quitting because another phil died.
-		philosopher->times_eaten++;
-		if (philosopher->times_eaten == data->times_to_eat)
-		{
-			mutex_lock(&data->philosophers_eating_mutex);
-			data->philosophers_eating--;
-			mutex_unlock(&data->philosophers_eating_mutex);
-		}
-
-		print_event(EVENT_EAT, philosopher, data);
-
-		mutex_lock(&philosopher->time_of_last_meal_mutex);
-		philosopher->time_of_last_meal = get_time_ms();
-		mutex_unlock(&philosopher->time_of_last_meal_mutex);
-
-		precise_sleep(philosopher->time_of_last_meal, data->time_to_eat, data);
-
-		// TODO: Does the unlocking need to occur in the same order as the locking? If so, then this won't work:
-
-		mutex_unlock(philosopher->left_fork);
-		mutex_unlock(philosopher->right_fork);
-
-		print_event(EVENT_SLEEP, philosopher, data);
-
-		// mutex_lock(&data->printf_mutex);
-		// printf("time to sleep: %zu\n", data->time_to_sleep);
-		// mutex_unlock(&data->printf_mutex);
-
-		precise_sleep(get_time_ms(), data->time_to_sleep, data);
-
-		// mutex_lock(&data->printf_mutex);
-		// printf("%zu after sleeping\n", philosopher->index + 1);
-		// mutex_unlock(&data->printf_mutex);
-
-		print_event(EVENT_THINK, philosopher, data);
-
+		if (should_stop(data))
+			return ;
+		grab_forks(philosopher, data);
+		eat(philosopher, data);
+		drop_forks(philosopher);
+		ph_sleep(philosopher, data);
+		think(philosopher, data);
 		if (data->philosopher_count % 2 == 1)
 			precise_sleep(get_time_ms(), data->time_to_eat / 2, data);
 	}
